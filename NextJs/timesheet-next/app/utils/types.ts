4cf59b006b6dbfb8ad36bf039e6c9df2807d9cbf
Ryan Reabel <ryanreabel@gmail.com>
2024-05-03 00:21:10 -0400

    converting table to a specific UI element

--- NextJs/timesheet-next/app/utils/types.ts
+++ NextJs/timesheet-next/app/utils/types.ts
@@ -1,5 +1,31 @@
+//type cannot be extended whereas interface can be.
 export type entry = {
-    description: string,
-    recurring: boolean,
-    allotedTime: number,
+    id: number;
+    description?: string,
+    recurring?: boolean,
+    allotedTime?: number,
+    date?: string;
+    start?: string;
+    end?: string;
+    break?: number;
+    project?: string;
+};
+
+//type for users
+export type user = {
+    id: number;
+    username: string;
+    password: string;
+    email: string;
+    role: string;
+};
+
+// History type
+export type history = {
+    id: number;
+    date: string;
+    description: string;
+    project: string;
+    hours: number;
+    user: string;
 };
---
+++ NextJs/timesheet-next/public/test-data.ts
@@ -0,0 +1,5 @@
+import { entry } from "@/app/utils/types"
+
+export let testData: Array<entry> =  [
+    { id: 1, date: "2021-01-01", start: "09:00", end: "17:00", break: 1, project: "Project 1", description: "Description 1" },
+]
