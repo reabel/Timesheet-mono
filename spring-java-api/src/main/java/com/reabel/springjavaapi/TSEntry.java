b95bc23c02b1322e7140db31a9e4b90e9fafc4d1
reabel <ryanreabel@gmail.com>
2024-05-07 23:14:04 -0400

    Rename end column to tsend to avoid issues within SQL

--- spring-java-api/src/main/java/com/reabel/springjavaapi/TSEntry.java
+++ spring-java-api/src/main/java/com/reabel/springjavaapi/TSEntry.java
@@ -1,5 +1,6 @@
 package com.reabel.springjavaapi;
 
+import java.util.Objects;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -12,19 +13,19 @@ class TSEntry {
   private Integer allotedTime;
   private String date;
   private String start;
-  private String end;
+  private String tsend;
   private Integer breakAmt;
   private String project;
 
   TSEntry() {}
 
-  TSEntry(String description, Boolean recurring, Integer allotedTime, String date, String start, String end, Integer breakAmt, String project) {
+  TSEntry(String description, Boolean recurring, Integer allotedTime, String date, String start, String tsend, Integer breakAmt, String project) {
     this.description = description;
     this.recurring = recurring;
     this.allotedTime = allotedTime;
     this.date = date;
     this.start = start;
-    this.end = end;
+    this.tsend = tsend;
     this.breakAmt = breakAmt;
     this.project = project;
   }
@@ -54,7 +55,7 @@ class TSEntry {
   }
 
   public String getEnd() {
-    return this.end;
+    return this.tsend;
   }
 
   public Integer getBreakAmt() {
@@ -90,7 +91,7 @@ class TSEntry {
   } 
 
   public void setEnd(String end) {
-    this.end = end;
+    this.tsend = end;
   }
 
   public void setBreakAmt(Integer breakAmt) {
@@ -108,17 +109,17 @@ class TSEntry {
     if (!(o instanceof TSEntry))
       return false;
     TSEntry entry = (TSEntry) o;
-    return java.util.Objects.equals(this.id, entry.id) && java.util.Objects.equals(this.description, entry.description) && java.util.Objects.equals(this.recurring, entry.recurring) && java.util.Objects.equals(this.allotedTime, entry.allotedTime) && java.util.Objects.equals(this.date, entry.date) && java.util.Objects.equals(this.start, entry.start) && java.util.Objects.equals(this.end, entry.end) && java.util.Objects.equals(this.breakAmt, entry.breakAmt) && java.util.Objects.equals(this.project, entry.project);
+    return Objects.equals(this.id, entry.id) && Objects.equals(this.description, entry.description) && Objects.equals(this.recurring, entry.recurring) && Objects.equals(this.allotedTime, entry.allotedTime) && Objects.equals(this.date, entry.date) && Objects.equals(this.start, entry.start) && Objects.equals(this.tsend, entry.tsend) && Objects.equals(this.breakAmt, entry.breakAmt) && Objects.equals(this.project, entry.project);
   }
   
   @Override
   public int hashCode() {
-    return java.util.Objects.hash(this.id, this.description, this.recurring, this.allotedTime, this.date, this.start, this.end, this.breakAmt, this.project);
+    return Objects.hash(this.id, this.description, this.recurring, this.allotedTime, this.date, this.start, this.tsend, this.breakAmt, this.project);
   }
 
   @Override
   public String toString() {
-    return "Entry{" + "id=" + this.id + ", description='" + this.description + '\'' + ", recurring=" + this.recurring + ", allotedTime=" + this.allotedTime + ", date='" + this.date + '\'' + ", start='" + this.start + '\'' + ", end='" + this.end + '\'' + ", breakAmt=" + this.breakAmt + ", project='" + this.project + '\'' + '}';
+    return "Entry{" + "id=" + this.id + ", description='" + this.description + '\'' + ", recurring=" + this.recurring + ", allotedTime=" + this.allotedTime + ", date='" + this.date + '\'' + ", start='" + this.start + '\'' + ", end='" + this.tsend + '\'' + ", breakAmt=" + this.breakAmt + ", project='" + this.project + '\'' + '}';
   }
 
 }
