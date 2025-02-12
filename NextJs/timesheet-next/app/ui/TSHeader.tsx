59617fe0ee4dbe7b35775910dddabae0a6659740
Ryan Reabel <ryanreabel@gmail.com>
2025-02-12 00:29:40 -0500

    header and landing page refactor.

--- NextJs/timesheet-next/app/ui/TSHeader.tsx
+++ NextJs/timesheet-next/app/ui/TSHeader.tsx
@@ -4,15 +4,19 @@ import { connectionStatus } from "../utils/types";
 const TSHeader = () => {
     return (
         <header className="flex items-center bg-gray-800 p-1">
-            <a href="/entry" className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 ">Add Entry</a>
+            <a href="/" className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4">Home :D</a>
             <a href="/dashboard" className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4">Dashboard</a>
+            <a href="/entry" className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 ">Add Entry</a>
             <a href="/admin" className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4">Admin</a>
             <div className="absolute right-0 ">
                 <div className="flex items-center justify-between">
-                    <TSAPIConnection connstatus={connectionStatus.PENDING}></TSAPIConnection>
-                    <button className="px-4 py-2 text-white bg-blue-500 mx-5">
-                    Refresh
+                    <button className="flex items-center justify-between px-4 py-2 text-white bg-blue-500">
+                        <TSAPIConnection connstatus={connectionStatus.DISCONNECTED}></TSAPIConnection>
+                        Refresh
                     </button>
+                    <a href="/next" className="px-4 py-2 text-white bg-red-500 mx-5">
+                        Built in Next.js
+                    </a>
                 </div>
             </div>
             <div>
